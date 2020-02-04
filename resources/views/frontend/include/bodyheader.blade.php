ca6c83c1f2b5e3250cfd947bc3f703a711668a9c
kartikdesai123 <kartikdesai123@gmail.com>
2020-02-04 18:54:33 +0530

    chagnes

--- resources/views/frontend/include/bodyheader.blade.php
+++ resources/views/frontend/include/bodyheader.blade.php
@@ -5,9 +5,9 @@ $currRoute = Route::current()->getName();
     <div class="container">
         <div class="row">
             <nav class="navbar navbar-brand navbar-expand-lg navbar-dark fixed-top" style="background-color: #000000">
-                <a class="logo_light" href="{{ route('dashboard') }}"><img style="height: 200px;" src="{{ url('public/frontend/assets/images/icons/R7.png') }}"></a>
-                <a class="logo_dark" href="{{ route('dashboard') }}"><img style="height: 150px;" src="{{ url('public/frontend/assets/images/icons/R8.png') }}"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <a class="logo_light" href="{{ route('dashboard') }}"><img  src="{{ url('public/frontend/assets/images/icons/R7.png') }}"></a>
+                <a class="logo_dark" href="{{ route('dashboard') }}"><img style="padding-left: 10px" src="{{ url('public/frontend/assets/images/icons/R8.png') }}"></a>
+                <button class="navbar-toggler" style="float: right;" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
